b7f955aebe93e2a555decf834b60437c7e98bad4
Alex Bush <alexabush7@gmail.com>
2018-06-20 09:16:02 -0700

    got prototype of ascii hangman working

--- components/DisplayHangman.js
+++ components/DisplayHangman.js
@@ -15,17 +15,49 @@ import { StyledView, StyledLetter } from '../styledComponents';
 
 const HangmanView = styled.View`
   min-height: 30%;
-  padding: 50px;
+  padding: 10px;
 `;
 
-const BODYPARTS = ['O', '|', '/', '\', ' | (', ' / '')];
+function getASCIIChar(asciiNum) {
+  return String.fromCharCode(asciiNum);
+}
+
+const dash = getASCIIChar(45);
+const underscore = getASCIIChar(95);
+const vertical = getASCIIChar(124);
+const leftLimb = getASCIIChar(47);
+const rightLimb = getASCIIChar(92);
+
+const GALLOWS = [
+  [' ', dash, dash, dash, vertical],
+  [' ', vertical, ' ', ' ', vertical],
+  [' ', 'O', ' ', ' ', vertical],
+  [leftLimb, vertical, rightLimb, ' ', vertical],
+  [' ', vertical, ' ', ' ', vertical],
+  [leftLimb, ' ', rightLimb, ' ', vertical],
+  [' ', ' ', underscore, underscore, underscore]
+];
 
 class DisplayHangman extends PureComponent {
   render() {
-    const bodyArr = BODYPARTS.slice(0, this.props.numWrongGuesses);
-    const display = bodyArr.map((part, index) => {
-      return <StyledLetter key={index}>{part}</StyledLetter>;
+    // const bodyArr = BODYPARTS.slice(0, this.props.numWrongGuesses);
+    // const display = bodyArr.map((part, index) => {
+    //   return <StyledLetter key={index}>{part}</StyledLetter>;
+    // });
+    let gallows = GALLOWS;
+    let joinedRows = gallows.map((row, index) => {
+      console.log('row: ', row);
+      return (
+        <Text
+          style={{ fontSize: 36, width: 100, backgroundColor: 'powderblue' }}
+          key={index}
+        >
+          {row.join('')}
+        </Text>
+      );
     });
+    let display = joinedRows;
+    debugger;
     return (
       <HangmanView>
         <View>{display}</View>
